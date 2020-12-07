20bcd7007f0f6053a9ca7f89fcae75f5a005a9cc
diptivm <diptivm08@gmail.com>
2020-12-07 14:27:06 +0000

    chore: track/ untrack files

--- build/pick_objects/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/catkin_ws/home-service-robot/src/pick_objects
-# Build directory: /home/workspace/catkin_ws/home-service-robot/build/pick_objects
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot_simulator/turtlebot_stage/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/catkin_ws/home-service-robot/src/turtlebot_simulator/turtlebot_stage
-# Build directory: /home/workspace/catkin_ws/home-service-robot/build/turtlebot_simulator/turtlebot_stage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
