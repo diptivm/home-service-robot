e26e4033672dc26b89cbb8cccdd17c8361f796c1
diptivm <diptivm08@gmail.com>
2020-12-11 17:47:56 +0000

    fix: relative paths in bash scripts

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/Georgia_studio.world" & 
+xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/../map/Georgia_studio.world" & 
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/gmap/map.yaml" &
+xterm -e "source ../../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find pick_objects)/../map/map.yaml" &
 sleep 5
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" 
+xterm -e "source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" 
 
 
